5040badaeb2403bb459a44579e6e27224027bcbf
Helza Aragão <helza.batista.aragao@gmail.com>
2025-03-12 18:14:26 -0300

    Corrigindo caminhos das imagens

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {App} from './App.tsx'
 
-import {HashRouter} from 'react-router-dom'
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HashRouter>
+    
       <App />
-    </HashRouter>
+
   </React.StrictMode>
 )
